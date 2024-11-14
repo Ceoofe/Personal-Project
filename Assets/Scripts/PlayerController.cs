e0e219fb68fc4ffd6950e28a2799beca819379c2
Ceoofe <146845430+Ceoofe@users.noreply.github.com>
2024-11-14 12:04:53 -0500

    Player Control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public float speed = 5.0f;
+    private float horizontalInput;
+    private int xMax = 10;
+    private int xMin = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    //Limits from the x-axis 10 to -10
+        if (transform.position.x > xMax)
+        {
+            transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < xMin)
+        {
+            transform.position = new Vector3(xMin, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody clone;
+            clone = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), bullet.transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+            Debug.Log("Shoot");
+        }
+    }
+}
